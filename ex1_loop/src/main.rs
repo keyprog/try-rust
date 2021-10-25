24ec9b62976d1f71d85fcf5a3e99b829932fd11a
Alex Pokislyuk <keyprog@gmail.com>
2021-10-25 19:41:33 -0400

    fibonacci

--- ex1_loop/src/main.rs
+++ ex1_loop/src/main.rs
@@ -1,3 +1,33 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Fibonacci number. v.0.1");
+    let mut input = String::new();    
+
+    loop {
+        print!("Input a number: ");
+        io::stdout().flush().unwrap();
+        
+        input.clear();
+        io::stdin().read_line(&mut input).unwrap();
+        let input : i32 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("A number was expected");
+                continue;
+            },
+        };
+        println!("Result: {}", fib(input));
+
+    };
+}
+
+fn fib(num: i32) -> i32 {
+    match num {
+        0 => return 0,
+        1 => return 1,
+        2 => return 1,
+        _ => return fib(num - 1)+ fib(num - 2),
+    }
 }
