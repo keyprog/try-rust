64504e80f7fd28f1f2b02aaec9fdfe8051fec6a2
Alex Pokislyuk <keyprog@gmail.com>
2021-11-13 10:31:23 -0500

    quick sort

--- home2/sort/src/quick_sort.rs
+++ home2/sort/src/quick_sort.rs
@@ -4,9 +4,9 @@ pub struct QuickSort {
 
 impl QuickSort {    
     pub fn exec_sort(mut input: &mut Vec<String>){
-        for line in input.clone() {
-            println!("{}", line);
-        }
+        //for line in input.clone() {
+            //println!("{}", line);
+        //}
 
         let end = input.len();
         Self::quick_sort(&mut input, 0, end);
@@ -20,22 +20,24 @@ impl QuickSort {
             return;
         }
         
-        let pi = Self::partition(&mut input, start, end);
+        let pi = Self::partition(&mut input, start, end-1);
 
         Self::quick_sort(&mut input, start, pi);
         Self::quick_sort(&mut input, pi+1, end);
     }
 
     fn partition(input: &mut Vec<String>, start: usize, end: usize) -> usize {
-        let piv = input[];
-        for i in start+1..end {
-            if input[pi] > input[i] {
-                input.swap(pi, i);                
-                pi = i;
+        let piv = input[end].clone();
+        let mut low = start;
+        for i in start..end {
+            if input[i] < piv {                
+                input.swap(low, i);                
+                low = low + 1;
             }
 
         }
-        return pi;
+        input.swap(low, end);
+        return low;
     }
 
 
